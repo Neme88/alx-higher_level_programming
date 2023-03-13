c712e950b484e53bae775dda106b118ca283ca83
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-03-13 19:14:03 +0200

    tests folder

--- 0x03-python-data_structures/tests/11-main.py
+++ 0x03-python-data_structures/tests/11-main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-delete_at = __import__('11-delete_at').delete_at
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
-
+  2 delete_at = __import__('11-delete_at').delete_at
+  3
+  4 my_list = [1, 2, 3, 4, 5]
+  5 idx = 3
+  6 new_list = delete_at(my_list, idx)
+  7 print(new_list)
+  8 print(my_list)
