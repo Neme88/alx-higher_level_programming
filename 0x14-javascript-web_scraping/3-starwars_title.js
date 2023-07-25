de30e6925f782fb37257cf977b49abd219db148e
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-07-25 18:21:11 +0200

    fixed bug on file 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/:id' + process.argv[2];
+let url = https:'https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2];
+
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
